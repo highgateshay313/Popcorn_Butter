fffeb0e67808de225983616f734811119ec9cda2
Shay Highgate <highgateshay@yahoo.com>
2026-05-11 10:29:18 -0500

    Fixed navigation

--- src/components/Navigation/List.jsx
+++ src/components/Navigation/List.jsx
@@ -4,12 +4,7 @@ import styles from './List.module.css';
 function List() {
     return (
         <div className={styles.list}>
-              <ul className={styles.list_items}>
-                    <li className={styles.item}>In Theaters</li>
-                    <li className={styles.item}>Popular</li>
-                    <li className={styles.item}>Most Rated</li>
-                    <li className={styles.item}>PG13 Movies</li>
-                </ul>
+          
         </div>
     )
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,13 +10,6 @@ function Navigation({logo}) {
             <div className={styles.logo_div}>
                 <h1 className={styles.logo_title}>{logo}</h1>
                 <img className={styles.app_logo} src={Logo} alt="" />
-                <div className={styles.menu_list}>
-                    <ul className={styles.menu_items}>
-                        <li className={styles.menu_item}>Menu</li>
-                        <li className={styles.menu_item}>Language</li>
-                        <li className={styles.menu_item}>Genre</li>
-                    </ul>
-                </div>
             </div>  
         </section>
     )
